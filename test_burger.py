ed15d575b5531dceb5ce697ba29c665251c41af8
Ivan Marinenkov <marinenkov.i.a@yandex.ru>
2026-08-17 12:37:21 +0500

    Написал тесты инициализации класса Burger

---
+++ test_burger.py
@@ -0,0 +1,17 @@
+import pytest
+from unittest.mock import Mock
+
+from praktikum.burger import Burger
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+
+class TestBurgerInit:
+    """Тесты инициализации класса Burger"""
+
+    def test_init_bun_is_none(self):
+        burger = Burger()
+        assert burger.bun is None
+
+    def test_init_ingredients_is_empty(self):
+        burger = Burger()
+        assert burger.ingredients == []
